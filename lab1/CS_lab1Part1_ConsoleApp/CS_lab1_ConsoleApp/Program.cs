2c2509db9313cbb084a20eca10d1b5d69fffbeea
Andriy Markus <andr98m@gmail.com>
2018-03-20 03:14:41 +0200

    lab1

---
+++ lab1/CS_lab1Part1_ConsoleApp/CS_lab1_ConsoleApp/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_lab1Part1_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Reading file
+            string pathInput = "input.txt";
+            string pathOutput = "output.txt";           
+            Console.WriteLine("Input file name/full path (press enter to use input.txt)");
+            string inp = Console.ReadLine();
+            if (inp != "")
+            {
+                pathInput = inp;
+                pathOutput = inp + "_report.txt";
+            }
+
+            string allLines;
+            using (StreamReader inputFileStream = new StreamReader(pathInput, Encoding.UTF8))
+            {
+                allLines = inputFileStream.ReadToEnd();
+            }
+            Console.WriteLine(allLines);
+
+            //Processing file
+            Console.WriteLine("Writing to " + pathOutput);
+            StreamWriter output = new StreamWriter(pathOutput, encoding: Encoding.UTF8, append: false);
+            Dictionary<char, int> charcounts = new Dictionary<char, int>();
+            int charNum = 0;
+            foreach(char ch in allLines)
+            {
+                //if(Char.IsLetter(ch))
+                {
+                    if (charcounts.ContainsKey(/*Char.ToLower*/(ch)))
+                        charcounts[/*Char.ToLower*/(ch)]++;
+                    else
+                        charcounts[/*Char.ToLower*/(ch)] = 1;
+                    charNum++;
+                }
+            }
+            output.WriteLine("Number of chars: "+charNum);
+            output.WriteLine("Char counts: ");
+            foreach (var kvp in charcounts.OrderBy((a) => a.Key))
+            {
+                output.WriteLine(kvp.Key + " --> " + kvp.Value);
+            }
+
+            //calculate char frequencies
+            Dictionary<char, double> charFrequencies = new Dictionary<char, double>();
+            foreach (var kvp in charcounts.OrderBy((a) => a.Key))
+            {
+                charFrequencies[kvp.Key] = (double)kvp.Value / (double)charNum;
+            }
+            output.WriteLine();
+            output.WriteLine("Char frequencies: ");
+            foreach (var kvp in charFrequencies.OrderBy((a) => a.Key))
+            {
+                output.WriteLine(kvp.Key + " --> " + kvp.Value);
+            }
+            // Average entropy    
+            output.WriteLine();
+            double avgEntropy = 0;
+            foreach(var kvp in charFrequencies)
+            {
+                avgEntropy += kvp.Value * Math.Log(1 / kvp.Value, 2);
+            }
+            output.WriteLine("Average entropy: " + avgEntropy);
+
+            // amount of information
+            output.WriteLine();
+            output.WriteLine("Amount of information: " + (avgEntropy * charNum));
+            output.WriteLine("Amount of information (divided by 8): " + (avgEntropy * charNum) / 8.0);
+            output.WriteLine("File size in bytes: "+ new System.IO.FileInfo(pathInput).Length);
+
+            //
+            output.Close();
+            Console.WriteLine("Done");
+            System.Threading.Thread.Sleep(500);
+        }
+    }
+}
---
+++ lab1/CS_lab1Part2_ConsoleApp/CS_lab1Part2_ConsoleApp/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_lab1Part2_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathInput = "input.txt";
+            string pathOutput = "output.txt";
+            Console.WriteLine("Encode in base64\nInput file name/full path (press enter to use input.txt):");
+            string inp = Console.ReadLine();
+            if (inp != "")
+            {
+                pathInput = inp;
+                pathOutput = inp + "_b64.txt";
+            }
+            string pattern = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+            StringBuilder encodingBuilder = new StringBuilder();
+            string b64 = "";
+            using (FileStream inputFileStream = new FileStream(pathInput,FileMode.Open,FileAccess.Read))
+            {
+                byte[] buff;
+                byte index = 0;
+                int amountread = 0;
+                do
+                {
+                    buff = new byte[3];
+                    amountread = inputFileStream.Read(buff, 0, buff.Length);
+                    if (amountread == 0) break;
+                    for (int i = 0; i < buff.Length * 8; i++)
+                    {
+                        if ((buff[i / 8] & ((byte)1 << 7 - i % 8)) != 0) // on position i is one
+                            index = (byte)(index | (byte)1 << (5 - i % 6));
+                        if((i+1) % 6 == 0)
+                        {
+                            if(i - 5  <  amountread * 8)
+                                encodingBuilder.Append(pattern[index]);                           
+                            else
+                                encodingBuilder.Append('=');
+                            index = 0;
+                        }
+                    }
+
+                } while (amountread >= 3);
+                inputFileStream.Position = 0;
+                buff = new byte[inputFileStream.Length];
+                inputFileStream.Read(buff, 0 , buff.Length);
+                b64 = Convert.ToBase64String(buff);
+            }
+            Console.WriteLine("Writing to " + pathOutput);
+            StreamWriter output = new StreamWriter(pathOutput, encoding: Encoding.UTF8, append: false);
+            if (b64 != encodingBuilder.ToString())
+                output.Write("Wrong encoded base 64");
+            else
+                output.Write(encodingBuilder);
+            output.Close();
+            Console.WriteLine("Done");
+            System.Threading.Thread.Sleep(500);
+        }
+    }
+}
